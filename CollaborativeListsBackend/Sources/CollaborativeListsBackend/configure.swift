3905fb890ae2ae3fce468e4c7ad488c9e31dd124
Anastasiia34 <mainstreamainstream@gmail.com>
2026-05-04 12:20:58 +0400

    chore(backend): remove boilerplate comments

--- CollaborativeListsBackend/Sources/CollaborativeListsBackend/configure.swift
+++ CollaborativeListsBackend/Sources/CollaborativeListsBackend/configure.swift
@@ -3,7 +3,6 @@ import Fluent
 import FluentPostgresDriver
 import Vapor
 
-// configures your application
 public func configure(_ app: Application) async throws {
 	app.http.server.configuration.hostname = "0.0.0.0"
 	app.http.server.configuration.port = 8080
